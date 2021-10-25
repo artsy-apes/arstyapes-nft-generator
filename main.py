c88b99272f653f72d23e945b6e05fd5624a58575
Tomaz Mesarec <tomaz.mesarec@student.tugraz.at>
2021-10-25 11:01:45 +0200

    Selecting image traits

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+import json
+import random
+import pprint as pp
+
+
+def select_image_attributes(config, weights=None):
+    image = dict()
+    attributes = config['attributes']
+
+    for att in attributes:
+        image[att] = random.choices(attributes[att]["images"], attributes[att]['weights'], k=1);
+
+    return image
+
+
 def main():
-    pass
+    with open('config.json') as f:
+        config = json.load(f)
+
+    for _ in range(config["total_images"]):
+        image = select_image_attributes(config)
+
 
 if __name__ == '__main__':
     main()
