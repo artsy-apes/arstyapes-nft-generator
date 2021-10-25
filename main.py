d468a69c3043d4753a34817d2229b19c8413c37d
tomazmm <tomaz.mesarec@gmail.com>
2021-10-25 17:04:08 +0200

    Added eye trait

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ import json
 import os
 import random
 import pprint as pp
+from collections import OrderedDict
+
 from PIL import Image
 
 
 def select_image_attributes(attributes):
-    image = dict()
+    image = OrderedDict()
 
     for att in attributes:
-        image[att] = random.choices(attributes[att]["images"], attributes[att]['weights'])[0]
+        image[att] = random.choices(attributes[att]['images'], attributes[att]['weights'])[0]
 
     return image
 
@@ -21,10 +23,12 @@ def generate_images(images: list):
         background = Image.open(f'assets/background/{img["background"]}.png').convert('RGBA')
         outfit = Image.open(f'assets/outfit/{img["outfit"]}.png').convert('RGBA')
         head = Image.open(f'./assets/head/{img["head"]}.png').convert('RGBA')
+        eye = Image.open(f'./assets/eye/{img["eye"]}.png').convert('RGBA')
         headwear = Image.open(f'./assets/headwear/{img["headwear"]}.png').convert('RGBA')
 
         ape = Image.alpha_composite(background, outfit)
         ape = Image.alpha_composite(ape, head)
+        ape = Image.alpha_composite(ape, eye)
         ape = Image.alpha_composite(ape, headwear)
 
         rgb = ape.convert('RGBA')
