506d39a112d829f948ffe3ecdcbd17ab84df8cf6
tomazmm <tomaz.mesarec@gmail.com>
2021-11-09 17:31:04 +0100

    Added shitcode for special case apes.

--- main.py
+++ main.py
@@ -1,42 +1,71 @@
 import json
 import os
+import sys
 import random
 import pprint as pp
-from collections import OrderedDict
 
 from PIL import Image
 
 
 def select_image_attributes(attributes):
-    image = OrderedDict()
-
+    image = dict()
+    is_gasmask = False
+    is_squid_game = False
+    is_zombie = False
     for att in attributes:
         image[att] = random.choices(attributes[att]['images'], attributes[att]['weights'])[0]
+        if att == "body":  # take the head with same color
+            color = image[att].split("-")[0]
+            image["head"] = f"{color}-head"
+        if "zombie" in image[att]:  # Zombie head has custom eyes
+            is_zombie = True
+        if "squidgame" in image[att]:  # Squidgame ape has only background and outfit
+            is_squid_game = True
+        if "gasmask" in image[att]: # Remove jewrely, mouth trait and eyes
+            is_gasmask = True
+
+    if is_zombie:
+        del image["eye"]
+
+    if is_squid_game:
+        for trait in list(image):
+            if trait == "background" or trait == "outfit":
+                continue
+            del image[trait]
+
+    if is_gasmask:
+        for trait in list(image):
+            if trait == "jewrely" or trait == "mouth attributes" or trait == "eye":
+                del image[trait]
 
     return image
 
 
 def generate_images(images: list):
+    traits_render_order = ["background", "body", "outfit", "head", "eye", "jewrely", "mouth attributes", "accessories", "headwear"]
     for i in range(len(images)):
-        img = images[i]
-
-        background = Image.open(f'assets/background/{img["background"]}.png').convert('RGBA')
-        outfit = Image.open(f'assets/outfit/{img["outfit"]}.png').convert('RGBA')
-        head = Image.open(f'./assets/head/{img["head"]}.png').convert('RGBA')
-        eye = Image.open(f'./assets/eye/{img["eye"]}.png').convert('RGBA')
-        headwear = Image.open(f'./assets/headwear/{img["headwear"]}.png').convert('RGBA')
-
-        ape = Image.alpha_composite(background, outfit)
-        ape = Image.alpha_composite(ape, head)
-        ape = Image.alpha_composite(ape, eye)
-        ape = Image.alpha_composite(ape, headwear)
+        ape = None
+        for trait in traits_render_order:
+            try:
+                trait_img = Image.open(f'assets/{trait}/{images[i][trait]}.png').convert('RGBA')
+                if ape is None:
+                    ape = trait_img
+                else:
+                    ape = Image.alpha_composite(ape, trait_img)
+            except Exception as e:
+                print(e)
 
         rgb = ape.convert('RGBA')
         file_name = str("artsyape-" + str(i)) + '.png'
 
         if not os.path.exists("generated"):
             os.mkdir('generated')
-        rgb.save("./generated/" + file_name)
+        rgb.save("./generated/" + file_name, optimize=True, quality=20)
+
+        # Count how many apes generated
+        sys.stdout.write("\r")
+        sys.stdout.write("{:2d} ape generated.".format(i))
+        sys.stdout.flush()
 
 
 def main():
