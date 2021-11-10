71539f527f6a6d895dd2634120535b60fd7e37d4
tomazmm <tomaz.mesarec@gmail.com>
2021-11-10 15:43:45 +0100

    Astronaut is a weird ape to generate

--- main.py
+++ main.py
@@ -1,49 +1,69 @@
+import enum
 import json
 import os
 import sys
 import random
 import pprint as pp
+from collections import OrderedDict
 
 from PIL import Image
 
 
 def select_image_attributes(attributes):
     image = dict()
+    traits = dict()
+    image["traits"] = traits
+    image["specialty"] = None
+    image["render_order"] = ["background", "body", "outfit",
+                             "head", "eye", "jewrely",
+                             "mouth attributes", "accessories", "headwear"]
+
     for att in attributes:
-        image[att] = random.choices(attributes[att]['images'], attributes[att]['weights'])[0]
+        trait = random.choices(attributes[att]['images'], attributes[att]['weights'])[0]
+        traits[att] = trait
         if att == "body":  # take the head with same color
-            color = image[att].split("-")[0]
-            image["head"] = f"{color}-head"
-        if "zombie" in image[att]:  # Zombie head has custom eyes
-            del image["eye"]
-        if "squidgame" in image[att]:  # Squidgame ape has only background and outfit
-            for trait in list(image):
-                if trait in ["background", "outfit"]:
-                    continue
-                del image[trait]
-        if "gasmask" in image[att]:  # Remove jewrely, mouth trait and eyes
-            for trait in list(image):
-                if trait in ["jewrely", "mouth attributes", "eyes"]:
-                    del image[trait]
-        # if "astronaut" in image[att]:
-        #     del image["headwear"]
-        #     image["astronaut_background"] = "astronaut 2"
+            color = traits[att].split("-")[0]
+            traits["head"] = f"{color}-head"
+        if "zombie" in traits[att]:  # Zombie head has custom eyes
+            image["specialty"] = "zombie"
+        if "squidgame" in traits[att]:  # Squidgame ape has only background and outfit
+            image["specialty"] = "squidgame"
+        if "gasmask" in traits[att]:  # Remove jewrely, mouth trait and eyes
+            image["specialty"] = "gasmask"
+        if "astronaut" in traits[att]:
+            image["specialty"] = "astronaut"
+
+    if image["specialty"] == "zombie":
+        del traits["eye"]
+    elif image["specialty"] == "squidgame":
+        for trait in list(traits):
+            if trait in ["background", "outfit"]:
+                continue
+            del traits[trait]
+    elif image["specialty"] == "gasmask":
+        for trait in list(traits):
+            if trait in ["jewrely", "mouth attributes", "eyes"]:
+                del traits[trait]
+    elif image["specialty"] == "astronaut":
+        for trait in list(traits):
+            if trait in ["jewrely", "mouth attributes", "headwear"]:
+                del traits[trait]
+        traits["body"] = "astronaut 2"
+        image["render_order"].append(image["render_order"].pop(2))  # render outfit as last
 
     return image
 
 
 def generate_ape_image(id_num: int, image: dict):
-    traits_render_order = ["background", "body", "outfit", "head", "eye", "jewrely", "mouth attributes", "accessories",
-                           "headwear"]
     ape = None
-    for trait in traits_render_order:
+    for trait in image["render_order"]:
         try:
-            trait_img = Image.open(f'assets/{trait}/{image[trait]}.png').convert('RGBA')
+            trait_img = Image.open(f'assets/{trait}/{image["traits"][trait]}.png').convert('RGBA')
             if ape is None:
                 ape = trait_img
             else:
                 ape = Image.alpha_composite(ape, trait_img)
-        except KeyError:
+        except KeyError as e:
             continue
 
     rgb = ape.convert('RGBA')
@@ -59,13 +79,9 @@ def generate_ape_image(id_num: int, image: dict):
     sys.stdout.flush()
 
 
-def generate_special_ape_image(special_type: str):
-    print(special_type)
-
-
 def main():
     with open('config.json') as f:
-        config = json.load(f)
+        config = json.load(f, object_pairs_hook=OrderedDict)
 
     images = list()
     it = 0
