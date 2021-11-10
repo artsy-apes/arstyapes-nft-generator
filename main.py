34c6e2bcc86af3398319e3042ac7f4ff78b12283
tomazmm <tomaz.mesarec@gmail.com>
2021-11-10 12:49:44 +0100

    Rendering everything except astronaut

--- main.py
+++ main.py
@@ -9,63 +9,58 @@ from PIL import Image
 
 def select_image_attributes(attributes):
     image = dict()
-    is_gasmask = False
-    is_squid_game = False
-    is_zombie = False
     for att in attributes:
         image[att] = random.choices(attributes[att]['images'], attributes[att]['weights'])[0]
         if att == "body":  # take the head with same color
             color = image[att].split("-")[0]
             image["head"] = f"{color}-head"
         if "zombie" in image[att]:  # Zombie head has custom eyes
-            is_zombie = True
+            del image["eye"]
         if "squidgame" in image[att]:  # Squidgame ape has only background and outfit
-            is_squid_game = True
-        if "gasmask" in image[att]: # Remove jewrely, mouth trait and eyes
-            is_gasmask = True
-
-    if is_zombie:
-        del image["eye"]
-
-    if is_squid_game:
-        for trait in list(image):
-            if trait == "background" or trait == "outfit":
-                continue
-            del image[trait]
-
-    if is_gasmask:
-        for trait in list(image):
-            if trait == "jewrely" or trait == "mouth attributes" or trait == "eye":
+            for trait in list(image):
+                if trait in ["background", "outfit"]:
+                    continue
                 del image[trait]
+        if "gasmask" in image[att]:  # Remove jewrely, mouth trait and eyes
+            for trait in list(image):
+                if trait in ["jewrely", "mouth attributes", "eyes"]:
+                    del image[trait]
+        # if "astronaut" in image[att]:
+        #     del image["headwear"]
+        #     image["astronaut_background"] = "astronaut 2"
 
     return image
 
 
-def generate_images(images: list):
-    traits_render_order = ["background", "body", "outfit", "head", "eye", "jewrely", "mouth attributes", "accessories", "headwear"]
-    for i in range(len(images)):
-        ape = None
-        for trait in traits_render_order:
-            try:
-                trait_img = Image.open(f'assets/{trait}/{images[i][trait]}.png').convert('RGBA')
-                if ape is None:
-                    ape = trait_img
-                else:
-                    ape = Image.alpha_composite(ape, trait_img)
-            except Exception as e:
-                print(e)
+def generate_ape_image(id_num: int, image: dict):
+    traits_render_order = ["background", "body", "outfit", "head", "eye", "jewrely", "mouth attributes", "accessories",
+                           "headwear"]
+    ape = None
+    for trait in traits_render_order:
+        try:
+            trait_img = Image.open(f'assets/{trait}/{image[trait]}.png').convert('RGBA')
+            if ape is None:
+                ape = trait_img
+            else:
+                ape = Image.alpha_composite(ape, trait_img)
+        except KeyError:
+            continue
 
-        rgb = ape.convert('RGBA')
-        file_name = str("artsyape-" + str(i)) + '.png'
+    rgb = ape.convert('RGBA')
+    file_name = str("artsyape-" + str(id_num)) + '.png'
 
-        if not os.path.exists("generated"):
-            os.mkdir('generated')
-        rgb.save("./generated/" + file_name, optimize=True, quality=20)
+    if not os.path.exists("generated"):
+        os.mkdir('generated')
+    rgb.save("./generated/" + file_name, optimize=True, quality=20)
 
-        # Count how many apes generated
-        sys.stdout.write("\r")
-        sys.stdout.write("{:2d} ape generated.".format(i))
-        sys.stdout.flush()
+    # Count how many apes generated
+    sys.stdout.write("\r")
+    sys.stdout.write("{:2d} ape generated.".format(id_num))
+    sys.stdout.flush()
+
+
+def generate_special_ape_image(special_type: str):
+    print(special_type)
 
 
 def main():
@@ -80,7 +75,10 @@ def main():
             images.append(image)
             it += 1
 
-    generate_images(images)
+    for i in range(len(images)):
+        generate_ape_image(i, images[i])
+
+    # generate_images(images)
 
 
 if __name__ == '__main__':
