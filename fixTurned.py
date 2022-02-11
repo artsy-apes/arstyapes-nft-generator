e6d09921991713590d52ea311395cf0490506c61
tomazmm <tomaz.mesarec@protonmail.com>
2022-02-11 14:30:33 +0100

    metaData to csv

--- fixTurned.py
+++ fixTurned.py
@@ -3,24 +3,27 @@ import os
 
 
 def main():
-    base_path = "generated/metadata_old"
-    base_path2 = "generated/metadata2"
+    base_path = "generated/metadata"
     dirs = os.listdir(base_path)
 
     last = None
     for file in dirs:
         path = os.path.join(base_path, file)
         try:
-            with open(path, 'rb') as f:
+            with open(path, 'r+') as f:
                 meta_data: dict = json.load(f)
                 for (trait_type, item) in meta_data["traits"].items():
-                    if trait_type == "outfit" and "Studo" in item:
-                        print(file)
+                    if trait_type == "headwear" and "Luart" in item:
+                        meta_data["traits"]["mouth"] = "Nothing"
+                        f.seek(0)
+                        f.write(json.dumps(meta_data, indent=4))
+                        f.truncate()
 
                 f.close()
 
         except Exception as e:
             print(e)
+            print(file)
 
 
 if __name__ == '__main__':
--- metaDataToCsv.py
+++ metaDataToCsv.py
@@ -8,13 +8,28 @@ import os
 # Opening JSON file and loading the data
 # into the variable data
 
+def metaDataToList():
+    base_path = "generated/metadata"
+    dirs = os.listdir(base_path)
+
+    ape_list = []
+    for file in dirs:
+        path = os.path.join(base_path, file)
+        try:
+            with open(path, 'rb') as f:
+                meta_data: dict = json.load(f)
+                ape_list.append(meta_data)
+
+        except Exception as e:
+            print(e)
+
+    return ape_list
+
 def main():
-    with open('list_metadata.json') as json_file:
-        apes = json.load(json_file)
+    apes = metaDataToList()
 
     # now we will open a file for writing
     data_file = open('metadata.csv', 'w')
-
     # create the csv writer object
     csv_writer = csv.writer(data_file)
 
@@ -27,7 +42,7 @@ def main():
             csv_writer.writerow(header)
             header = True
         ape_id = ape["id"]
-        values = [ape_id, f"ArtsyApe {ape_id}", f"artsyape-{ape_id}"]
+        values = [ape_id, f"ArtsyApe {ape_id}", f"artsyape-{ape_id}.jpeg"]
         values.extend(list(ape["traits"].values()))
         csv_writer.writerow(values)
 
